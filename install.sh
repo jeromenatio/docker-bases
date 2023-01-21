78b087c95e798466ba863bcdd720070d5e83a0e8
Jérôme Natio <natio.jerome@gmail.com>
2023-01-21 19:08:51 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,25 @@
 # The structure is container based (DOCKER)
 
 # FUNCTIONS
+spin() {
+  local text="$1"
+  local pid=$!
+  local spin='⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏'
+  local i=0
+  local check_character="✔"
+  local spin_color=96
+  local text_color=36
+  while kill -0 $pid 2>/dev/null
+  do
+    i=$(( (i+1) %10 ))
+    printf "\033[${text_color}m\r\033[${spin_color}m${spin:$i:1} \033[${text_color}m$text\033[0m"
+    sleep .1
+  done
+  printf "\033[${spin_color}m\r$check_character $text\033[0m\n"
+}
 
 # PARAMS
+sleep 10 && spin("Waiting in vain")
 
 # INSTALL DEPENDENCIES
 
