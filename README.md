e3eb016913c84e3ede99c8fbecd5ba371d340290
Jérôme Natio <natio.jerome@gmail.com>
2023-01-21 18:19:48 +0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # docker-bases
 Boiler files and scripts to install a docker based webserver. It includes reverse proxies and ssl certification.
+The script is for a debian based linux (debian, ubuntu ...)
+
+download install.sh
+make it executable
+execute and follow instructions
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This inscript will install the basics administration tools
+# for a web server on debian based linux architecture.
+# The structure is container based (DOCKER)
+
+# FUNCTIONS
+
+# PARAMS
+
+# INSTALL DEPENDENCIES
+
+# INSTALL DOCKER
+
+# CREATE DOCKER USER AND GROUP
+
+# DOWNLOAD .env
+
+# ASK USER SOME QUESTIONS AND MODIFY .env
+
+# CREATE DOCKER DIRECTORIES
+
+# INSTALL THE BASES CONTAINERS
+
+# DELETE ALL TEMPORARY FILES
+
+# DELETE THIS FILE
+
+# https://raw.githubusercontent.com/jeromenatio/docker-bases/main/nginxproxy.yml
