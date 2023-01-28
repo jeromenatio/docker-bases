ab6a33eb2eb7c100573112f633a247d63ff29743
Jérôme Natio <natio.jerome@gmail.com>
2023-01-28 16:09:33 +0400

    Guacamole done

--- install.sh
+++ install.sh
@@ -315,9 +315,7 @@ is_home_empty
 (tnexec "chown docker:docker $DOCKER_HOME -R" $logfile) & spin "Changing DOCKER HOME owner"
 
 # INSTALL THE BASES CONTAINERS
-#(tnexec "
-install_containers $envfile
-#" $logfile) & spin "Installing docker containers"
+(tnexec "install_containers $envfile" $logfile) & spin "Installing docker containers"
 
 # CHANGE OWNER OF DOCKER HOME
 (tnexec "chown docker:docker $DOCKER_HOME -R" $logfile) & spin "Changing DOCKER HOME owner"
