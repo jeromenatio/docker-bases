42360f41e0bb18f24a32d2e7e584ad63c1abebf6
Jérôme Natio <natio.jerome@gmail.com>
2023-01-30 10:32:27 +0400

    Bug fixe if statement

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ tnexec() {
 install_docker(){
     local logfile=$1
     (tnexec "apt-get update && apt-get install -y apt-transport-https ca-certificates gnupg lsb-release" $logfile) & spin "Installing docker dependencies"
-    if [ ! -e "/usr/share/keyrings/docker-archive-keyring.gpg" ]
+    if [ ! -e "/usr/share/keyrings/docker-archive-keyring.gpg" ]; then
         (tnexec "curl -fsSL -H 'Cache-Control: no-cache' https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg" $logfile) & spin "Downloading and saving docker key"
     fi
     (tnexec "echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable' | tee /etc/apt/sources.list.d/docker.list > /dev/null" $logfile) && spin "Modifying repositories source.list"
