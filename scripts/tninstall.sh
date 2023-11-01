0db06818db1e35e14b3135d809bb768be21d153d
jeromenatio <natio.jerome@gmail.com>
2023-11-01 15:59:23 +0400

    ++44

--- scripts/tninstall.sh
+++ scripts/tninstall.sh
@@ -48,7 +48,7 @@ tnIsCommandMissing docker-compose && tnInstallDockerCompose "$(uname -s)" "$(una
 
 # IF HOME ALREADY EXISTS STOP THE SCRIPT
 if [[ -d "$DOCKER_HOME" ]]; then
-    tnDisplay "Une installation '$DOCKER_HOME' existe déjà !!\n\n" "$darkYellowRedColor"
+    tnDisplay "Une installation '$DOCKER_HOME' existe déjà !!\n\n" "$darkYellowColor"
     exit 1    
 fi
 
